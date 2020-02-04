364f3fb32468059fb123a2ba4082461b43c402ad
mohamed Ragab <mohamed.Ragab1083@ci.menofia.edu.eg>
2020-02-05 00:37:51 +0200

    commit

---
+++ Leave-mangement/Models/LeaveTypeVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_mangement.Models
+{
+    public class DetailsLeaveTypeVm
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+    public class CreateLeaveTypeVm
+    {
+      
+        [Required]
+        public string Name { get; set; }
+      
+    }
+}
